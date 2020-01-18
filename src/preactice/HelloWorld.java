0b02584b0fafd608be6426943ec58f0ba66b6b05
parkhuijun <parkhuijun@localhost>
2020-01-18 19:30:32 +0900

    hi massage

--- src/preactice/HelloWorld.java
+++ src/preactice/HelloWorld.java
@@ -3,6 +3,6 @@ package preactice;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("aasd");
+		System.out.println("asd");
 	}
 }
